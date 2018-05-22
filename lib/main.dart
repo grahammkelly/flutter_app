e00079deb402bf78b0167269578381d36597ba73
gkelly <graham.kelly@travelportdigital.com>
2018-05-22 15:50:04 +0100

    Step 6.1: Added hamburger menu to the list sub-page (does nothing yet)

--- lib/main.dart
+++ lib/main.dart
@@ -83,8 +83,14 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold (
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _pushSaved() {
   }
 }
