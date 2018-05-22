f1cdc738fe11880405ae9a317de8596ba0e81e2a
gkelly <graham.kelly@travelportdigital.com>
2018-05-22 15:33:48 +0100

    Step 5: Added 'like' list and populate it when a word pair is clicked
    * Added a set for storing wordpairs in the RandomWordState
    * Added a trailing icon on the list to indicate if a wordpair is 'liked'
    * Added a 'click' action to list items to store/unstore the wordpairs

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,8 @@ class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  final _saved = new Set<WordPair>();
+
   Widget _buildSuggestions() {
     return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -54,11 +56,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
